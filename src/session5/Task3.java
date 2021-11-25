6d196a9079eebee94a7d2a1c1e3921acd1b01909
Fred Walther <alfred.walther@artingo.net>
2021-11-25 08:28:37 +0100

    Session 5: added tasks

---
+++ src/session5/Task3.java
@@ -0,0 +1,13 @@
+package session5;
+
+/**
+ * Use the code from transfer task 2 in this session which is used to do basic arithmetic operations with fractions.
+ * Add a constructor.
+ */
+public class Task3 {
+    public static void main(String[] args) {
+        // make this code run
+        // Fraction fraction = new Fraction(1, 2);
+        // System.out.println("fraction = " + fraction);
+    }
+}
